f7903dc07595af4dceb266dc7adc1886c3ddc5de
Keyvan Ebrahimpour <ebrahimp@arri.de>
2022-08-10 22:39:21 +0200

    Add boost to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <boost/filesystem/operations.hpp>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello CMake!" << std::endl;
+   std::cout << "The size of " << boost::filesystem::absolute(argv[0])
+             << " is " << boost::filesystem::file_size(argv[0]) << "\n"
+
    return 0;
 }
